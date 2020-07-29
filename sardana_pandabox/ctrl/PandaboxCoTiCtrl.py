589b2082b39013ce46d2cab5cb48dac1bd8aa392
Juliano Murari <juliano.murari@maxiv.lu.se>
2020-07-29 12:52:23 +0200

    Refactoring

--- sardana_pandabox/ctrl/PandaboxCoTiCtrl.py
+++ sardana_pandabox/ctrl/PandaboxCoTiCtrl.py
@@ -2,7 +2,7 @@
 from pandaboxlib import PandA
 import time
 import socket
-from multiprocessing.pool import ThreadPool
+from sockio.py2 import TCP 
 from StringIO import StringIO 
 import numpy as np 
 
@@ -58,6 +58,25 @@ class PandaboxCoTiCtrl(CounterTimerController):
         except (NameError, socket.gaierror):
             raise Exception('Unable to connect to PandABox.') 
 
+        # make sure PCAP block is reset
+        self.pandabox.query('PCAP.ENABLE=ZERO')
+        self.pandabox.query('*PCAP.DISARM=')
+
+        try:
+            self.data_socket = TCP(self.PandaboxHost, 8889, timeout=3)
+            self.data_socket.open()
+        except:
+            raise Exception('Unable to open PandABox data stream.') 
+ 
+        # check if data stream starts correctly
+        ack = self.data_socket.write_readline('\n')
+        if "OK" not in ack:
+            raise Exception('Acknowledge to data stream failed!') 
+        print "PandaboxCoTiCtrl: data stream listener starts...", ack
+        self.data_buffer = ""
+        self.header_okay_flag = False
+        self.data_end_flag = False 
+
         self.attributes = {}
         self.hw_trigger_cfg = {}
         self.hw_trigger_cfg['enable'] = self.PcapEnable
@@ -78,9 +97,6 @@ class PandaboxCoTiCtrl(CounterTimerController):
             'FMC_ACQ427_IN.VAL5','FMC_ACQ427_IN.VAL6','FMC_ACQ427_IN.VAL7',
             'FMC_ACQ427_IN.VAL8','PCAP.SAMPLES']
 
-        self.data_pool = ThreadPool(processes=1)
-        self.async_result = None
-
         self.index = 0
         self._repetitions = 0
 
@@ -95,6 +111,7 @@ class PandaboxCoTiCtrl(CounterTimerController):
     def DeleteDevice(self, axis):
         """Delete device from the controller."""
         self._log.debug("DeleteDevice(%d): Entering...", axis)
+        self.data_socket.close()
         self.pandabox.disconnect_from_panda()
 
     def StateAll(self):
@@ -131,32 +148,44 @@ class PandaboxCoTiCtrl(CounterTimerController):
 
         # Set Integration time in s per point
         self.pandabox.query('PULSE1.WIDTH.UNITS=s')
-        self.pandabox.query('PULSE1.WIDTH=%f' % (value))
+        if value < 8e-08:   # minimum reliable integration time is
+                            # 10 FPGA clock ticks 125 MHz -> 80 ns 
+            self._log.debug("The minimum integration time is 80 ns")
+            value = 8e-08
+        self.pandabox.query('PULSE1.WIDTH=%.9f' % (value))
+
+        # Set falling edge capture to have
+        # "gate signal that marks the capture boundaries"
+        # in order to respect the integration time
+        # see PCAP block documentation
+        self.pandabox.query('PCAP.TRIG_EDGE=Falling')
 
         if self._synchronization in [AcqSynch.SoftwareTrigger,
                                      AcqSynch.SoftwareGate]:
             # self._log.debug("SetCtrlPar(): setting synchronization "
             #                 "to SoftwareTrigger")
             self._repetitions = 1
+            self.pandabox.query('PULSE1.PULSES=1')
 
             # link blocks for software acquisition
-            # TODO: why PCAP.SAMPLES is zero with this layout 
-            enable = 'PULSE1.OUT'
-            gate = 'PULSE1.OUT'
             trig = 'PULSE1.OUT'
         else:
             # self._log.debug("SetCtrlPar(): setting synchronization "
             #                 "to HardwareTrigger")
             self._repetitions = repetitions
             # link blocks for hardware acquisition
-            enable = self.hw_trigger_cfg['enable']
-            gate = self.hw_trigger_cfg['gate']
             trig = self.hw_trigger_cfg['trig']
 
-        # create links
-        self.pandabox.query('PCAP.ENABLE='+enable)
-        self.pandabox.query('PCAP.GATE='+gate)
+        # create links to PCAP block
+        # TODO: separate gated mode? 
+        #self.pandabox.query('PCAP.GATE=ONE')
+        self.pandabox.query('PCAP.GATE='+trig)
         self.pandabox.query('PCAP.TRIG='+trig)
+        
+        # reset data buffer and header flag
+        self.header_okay_flag = False
+        self.data_end_flag = False 
+        self.data_buffer = '' 
 
     def PreStartOneCT(self, axis):
         # self._log.debug("PreStartOneCT(%d): Entering...", axis)
@@ -170,17 +199,21 @@ class PandaboxCoTiCtrl(CounterTimerController):
         PreStartOneCT for master channel.
         """
         # self._log.debug("StartAllCT(): Entering...")
-        cmd = '*PCAP.ARM='
-        self.pandabox.query(cmd)
+        ret = self.pandabox.query('*PCAP.ARM=')
+        if "OK" not in ret:
+            print "Pandabox arm PCAP failed. Disarm and arm again..."
+            ret = self.pandabox.query('*PCAP.DISARM=')
+            ret = self.pandabox.query('*PCAP.ARM=')
+
+        # start acquisition by enabling PCAP
+        self.pandabox.query('PCAP.ENABLE=ONE')
+
+        # trig acquisition
         if self._synchronization in [AcqSynch.SoftwareTrigger,
                                      AcqSynch.SoftwareGate]:
             self.pandabox.query('PULSE1.TRIG=ZERO')
             self.pandabox.query('PULSE1.TRIG=ONE')
-
-        try:
-            self.async_result = self.data_pool.apply_async(self.get_data, args = (self.PandaboxHost, 8889))
-        except Exception as e:
-            raise Exception("StartAll async thread error: %s: "+str(e))
+        # else wait for triggers (hardware mode)
 
         # THIS PROTECTION HAS TO BE REVIEWED
         # FAST INTEGRATION TIMES MAY RAISE WRONG EXCEPTIONS
@@ -196,56 +229,59 @@ class PandaboxCoTiCtrl(CounterTimerController):
 
     def ReadAll(self):
         # self._log.debug("ReadAll(): Entering...")
-        data_ready = int(self.pandabox.numquery('*PCAP.CAPTURED?'))
-#        print "Points acquired: %d"%data_ready
-
-        # get full result from acquisition
-        result = self.async_result.get()
-#        print "total result: ", result 
-
-        num_channels_enabled = self.pandabox.get_number_channels()
-
-        # HEADER FORMAT:
-        # missed: 0
-        # process: Scaled
-        # format: ASCII
-        # fields:
-        #  + one line per channel enabled
-        fixed_header_lines = 4 
-
-        lines = result.split('\n')
-        channels = lines[fixed_header_lines:fixed_header_lines+num_channels_enabled]
-        self.channels_order = []
-        for channel in channels:
-            channel = channel.split(' ')[1:2]
-            self.channels_order.append(channel[0])
-#        print self.channels_order
+        self.data_ready = int(self.pandabox.numquery('*PCAP.CAPTURED?'))
+        #print "Points acquired: %d"%self.data_ready
 
         self.new_data = [] 
-        self.index = 0 
+        #self.index = 0 
 
-        if self.index < data_ready:   # mandatory to avoid extra lines
-            data_only = lines[fixed_header_lines+num_channels_enabled+1:-2]
-            data_only = '\n'.join(data_only)
-#            data_only = np.loadtxt(StringIO(data_only), dtype='float')
-            data_only = np.genfromtxt(StringIO(data_only), dtype='float64')
-            self.new_data = np.ndarray.tolist(data_only.transpose())
-            if type(self.new_data[0]) != list:
-                one_line_data = []
-                for value in self.new_data:
-                    one_line_data.append([value])
-                self.new_data = one_line_data 
+        if self.data_ready == 0:
+            print "Pandabox: No data available yet."
+            self._ParseHeader()
+            return
+        elif self.data_ready <= self._repetitions:
+            if self.data_ready == self._repetitions:
+                print "Pandabox data acquisition has finished, disabling PCAP..."
+                self.pandabox.query('PCAP.ENABLE=ZERO') # it disarms PCAP too
+            try:
+                if not self.data_end_flag:
+                    data = self.data_socket.readline()
+                    if 'END' not in data:
+                        self.data_buffer += data
+                    else:
+                        print "Pandabox data acquisition ENDs okay!"
+                        self.data_end_flag = True 
+            except socket.error, e:
+                print "Pandabox: data socket error: ", e
+                self.data_socket.close()
 
-            time_data = [self.itime] * len(self.new_data[0])
-            self.new_data.insert(0, time_data)
-            
-            if self._repetitions != 1:
-                self.index += len(time_data)
+            data_only = np.genfromtxt(StringIO(self.data_buffer), dtype='float64')
+
+            # TODO: hw scan mode fails with int time < 100 ms
+            # we have only one line per execution of this method, fix it
+            if data_only.ndim > 1: # crop to get only new data
+                data_only = data_only[self.index:self.data_ready+1]
+
+            if self.index <= self.data_ready:   # mandatory to avoid extra lines
+                self.new_data = np.ndarray.tolist(data_only.transpose())
+                if type(self.new_data[0]) != list:
+                    one_line_data = []
+                    for value in self.new_data:
+                        one_line_data.append([value])
+                    self.new_data = one_line_data 
+    
+                time_data = [self.itime] * len(self.new_data[0])
+                self.new_data.insert(0, time_data)
+                
+                if self._repetitions != 1:
+                    self.index += len(time_data)
+
 
     def ReadOne(self, axis):
         # self._log.debug("ReadOne(%d): Entering...", axis)
         if len(self.new_data) == 0:
-            return []
+            #return -1 
+            return None 
 
         if axis == 1:    # timer axis
             channel_index = 0
@@ -258,7 +294,6 @@ class PandaboxCoTiCtrl(CounterTimerController):
             
         if self._synchronization in [AcqSynch.SoftwareTrigger,
                                      AcqSynch.SoftwareGate]:
-#            return SardanaValue(self.new_data[axis-1][0])
             return SardanaValue(self.new_data[channel_index][0])
 
         else:
@@ -269,22 +304,42 @@ class PandaboxCoTiCtrl(CounterTimerController):
         # self._log.debug("AbortOne(%d): Entering...", axis)
         self.pandabox.query('*PCAP.DISARM=')
 
-    # listener to tcp port where data is streamed from pandabox
-    def get_data(self, host, port):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((host, port))
-        s.sendall('\n')
-        data = s.recv(1024)
-        raw_data = ""
-        while True:
-    #        print 'data:', repr(data)
-            data = s.recv(1024)
-            raw_data += data 
-            if "END" in repr(data):
-                break
-        s.close()
-    #    print 'Received data:', repr(raw_data)
-        return raw_data
+    def StopOne(self, axis):
+        # self._log.debug("StopOne(%d): Entering...", axis)
+        self.pandabox.query('PCAP.ENABLE=ZERO')
+        self.pandabox.query('*PCAP.DISARM=')
+
+    def _ParseHeader(self):
+        if not self.header_okay_flag:
+            # Prepare for data receiving, parse header after arm
+            # HEADER FORMAT:
+            # missed: 0
+            # process: Scaled
+            # format: ASCII
+            # fields:
+            #  + one line per channel enabled
+            fixed_header_lines = 4 
+            num_channels_enabled = self.pandabox.get_number_channels()
+            num_lines = fixed_header_lines+num_channels_enabled
+    
+            data_header = ""
+            try:
+                data_header = self.data_socket.readlines(num_lines+1) #+1 blank line
+                if "fields" in data_header[3]:
+                    print "Pandabox data header parsing okay!"
+                    self.header_okay_flag = True 
+            except socket.error, e:
+                print "Pandabox: socket error header!!!! = ", e
+                self.header_okay_flag = False
+                pass 
+    
+            channels_list = data_header[fixed_header_lines:fixed_header_lines+num_channels_enabled]
+            self.channels_order = []
+            for channel in channels_list:
+                channel = channel.split(' ')[1:2]
+                self.channels_order.append(channel[0])
+            #print "Channels order: ", self.channels_order
+        return
 
 ###############################################################################
 #                Axis Extra Attribute Methods
@@ -335,30 +390,36 @@ class PandaboxCoTiCtrl(CounterTimerController):
         return value
 
 if __name__ == '__main__':
-    host = 'b308a-cab04-pandabox-temp-0'
-    enable = 'PCOMP2.OUT'
-    gate = 'LUT1.OUT'
-    trig = 'LUT1.OUT'
+    host = 'w-kitslab-pandabox-0'
+    enable = 'ONE'
+    gate = 'ONE'
+    trig = 'PULSE1.OUT'
     ctrl = PandaboxCoTiCtrl('test', {'PandaboxHost': host, 'PcapEnable': enable,'PcapGate': gate, 'PcapTrig': trig})
     ctrl.AddDevice(1)
     ctrl.AddDevice(2)
+    ctrl.SetExtraAttributePar(2, "ChannelName", "COUNTER1.OUT")
     ctrl.AddDevice(3)
-    ctrl.AddDevice(4)
-    ctrl.AddDevice(5)
+    ctrl.SetExtraAttributePar(3, "ChannelName", "INENC1.VAL")
+    #ctrl.AddDevice(4)
+    #ctrl.AddDevice(5)
 
+    acqtime = 0.5
     ctrl._synchronization = AcqSynch.SoftwareTrigger
-    # ctrl._synchronization = AcqSynch.HardwareTrigger
-    acqtime = 1.1
-    ctrl.LoadOne(1, acqtime, 10)
+    #ctrl._synchronization = AcqSynch.HardwareTrigger
+    if ctrl._synchronization == AcqSynch.SoftwareTrigger:
+        repetitions = 1
+    else:
+        repetitions = 3
+    ctrl.LoadOne(1, acqtime, repetitions)
     ctrl.StartAllCT()
     t0 = time.time()
     ctrl.StateAll()
     while ctrl.StateOne(1)[0] != State.On:
         ctrl.StateAll()
-        time.sleep(0.1)
-    print time.time() - t0 - acqtime
-    ctrl.ReadAll()
-    print ctrl.ReadOne(2)
-
+        ctrl.ReadAll()
+        time.sleep(0.25)
+    print "Time: ", time.time() - t0 - acqtime
+    print "COUNTER1.OUT = ", ctrl.ReadOne(2)
+    print "INENC1.VAL = ", ctrl.ReadOne(3)
 
 
